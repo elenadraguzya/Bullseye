6025f0bf198e22b70763beb243df8ced02d1068a
Olena <olena@PDF135-air.local>
2019-10-19 16:19:05 +0300

    solution

---
+++ Bullseye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Olena on 19.10.2019.
+//  Copyright © 2019 Elena Draguzya. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
