a096a38dbaaff54ecd289b7ff76cae1da3e90314
Olena <olena@PDF135-air.local>
2019-10-20 10:07:34 +0300

    UI

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -11,12 +11,24 @@ import SwiftUI
 struct ContentView: View {
     
     @State var alertIsVisible: Bool = false
-    
+    @State var sliderValue: Double = 50.0
     
     var body: some View {
         VStack {
-            Text("Welcome To My App")
-                .foregroundColor(Color.red)
+            Spacer()
+            HStack {
+                Text("Put the bullseye as close as you can to:")
+                Text("100")
+            }
+            
+            Spacer()
+            HStack {
+                Text("1")
+                Slider(value: self.$sliderValue, in: 1...100)
+                Text("100")
+            }
+            
+            Spacer()
             Button(action: {
                 print("Button Pressed")
                 self.alertIsVisible = true
@@ -24,15 +36,33 @@ struct ContentView: View {
                 Text(/*@START_MENU_TOKEN@*/"Hit Me"/*@END_MENU_TOKEN@*/)
             }
             .alert(isPresented: $alertIsVisible) { () -> Alert in
-                return Alert(title: Text("Hello There"), message: Text("This Is My Pop-Up"), dismissButton: .default(Text("Ok")))
-                
+                let roundedValue: Int = Int(self.sliderValue.rounded())
+                return Alert(title: Text("Hello There"), message: Text("The slider's value is \(roundedValue)."), dismissButton: .default(Text("Ok")))
             }
+            
+            Spacer()
+            HStack {
+                Button(action: {}) {
+                    Text("Start Over")
+                }
+                Spacer()
+                Text("Score:")
+                Text("999999")
+                Spacer()
+                Text("Round:")
+                Text("999")
+                Spacer()
+                Button(action: {}) {
+                    Text("Info")
+                }
+            }
+            .padding(.bottom, 20)
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewLayout(.fixed(width: 896, height: 414))
     }
 }
